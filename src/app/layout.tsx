e55cd2850ff6b4bd3447aca80ece3cfe63214764
Gowthamvelmurugan <gowthamjsb@gmail.com>
2026-09-14 23:24:20 +0530

    Explicitly define icon in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Vikram Traders | High-End Sivakasi Fireworks",
   description: "Premium fireworks at direct factory prices.",
+  icons: {
+    icon: "/icon.png",
+  }
 };
 
 export default function RootLayout({
